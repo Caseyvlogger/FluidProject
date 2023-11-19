43cf37d930013cb815249a454f9bfc02a7c14782
Caseyvlogger2311 <caseyvlogger2311@gmail.com>
2023-11-19 15:06:39 +0500

    launch screens, pkg config, removed lottie package

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-
-import {
-  StyleSheet,
-} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen'
+// import { } from 'react-native';
 import DiceScreen from './src/DiceScreen';
 
 function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <DiceScreen />
   );
 }
-
-const styles = StyleSheet.create({
-
-});
 
 export default App;
--- android/app/src/main/java/com/fluidproject/MainApplication.java
+++ android/app/src/main/java/com/fluidproject/MainApplication.java
@@ -1,4 +1,7 @@
 package com.fluidproject;
+//added for package
+// import org.devio.rn.splashscreen.SplashScreenReactPackage;
+
 
 import android.app.Application;
 import com.facebook.react.PackageList;
@@ -25,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          //added this line.
+          // packages.add(new SplashScreenReactPackage());
           return packages;
         }
 
